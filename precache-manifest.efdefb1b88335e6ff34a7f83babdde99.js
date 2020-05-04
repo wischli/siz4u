6498ece0a7d34aaa83852089edf55eb99bf6f974
wischli <w.freude@icloud.com>
2020-05-04 21:13:00 +0200

    Updates

---
+++ precache-manifest.efdefb1b88335e6ff34a7f83babdde99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d14bbad547907c7b6e45160963c0d4",
+    "url": "/siz4u/index.html"
+  },
+  {
+    "revision": "774ed008e1b78d1954ca",
+    "url": "/siz4u/static/css/main.64771185.chunk.css"
+  },
+  {
+    "revision": "4fc6af696254c1424c90",
+    "url": "/siz4u/static/js/2.184afd0c.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/siz4u/static/js/2.184afd0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774ed008e1b78d1954ca",
+    "url": "/siz4u/static/js/main.7fb88546.chunk.js"
+  },
+  {
+    "revision": "71656004981ee8bb5a4a",
+    "url": "/siz4u/static/js/runtime-main.2f981498.js"
+  }
+]);
